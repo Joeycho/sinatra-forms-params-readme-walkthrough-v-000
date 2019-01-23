0289f0b3cf6bbf58dc389ca0c7979868eb55c2e2
Joey Cho <cjy12182000@naver.com>
2019-01-23 06:48:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,10 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    "Hello, this is HTML Forms and Params"
+  end
+  
   get '/food_form' do
     erb :food_form
   end
 
   # Add your post route and action below
+  
+  
 
 end
